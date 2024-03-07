441ab357750c344e5deaf8b680c3766bf9f4a15b
Kichmarevitmo <kichmarev2003@gmail.com>
2024-03-07 12:08:38 +0300

    fix

--- src/main/java/fr/mossaab/security/service/StorageAdvantageService.java
+++ src/main/java/fr/mossaab/security/service/StorageAdvantageService.java
@@ -1,9 +1,11 @@
 package fr.mossaab.security.service;
 
+import org.springframework.stereotype.Service;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-
+@Service
 public class StorageAdvantageService {
     public byte[] downloadImageFromFileSystem(String fileName) throws IOException {
         byte[] images = Files.readAllBytes(new File("/var/www/vuary/IconsAdvantage/"+fileName).toPath());
