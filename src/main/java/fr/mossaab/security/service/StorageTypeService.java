0dcd877b3961c4be0e0a9fd0caf71c443763070e
Kichmarevitmo <kichmarev2003@gmail.com>
2024-03-07 12:05:55 +0300

    fix

--- src/main/java/fr/mossaab/security/service/StorageTypeService.java
+++ src/main/java/fr/mossaab/security/service/StorageTypeService.java
@@ -1,12 +1,14 @@
 package fr.mossaab.security.service;
 
 import fr.mossaab.security.entities.FileData;
+import org.springframework.stereotype.Service;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.Optional;
 
+@Service
 public class StorageTypeService {
     public byte[] downloadImageFromFileSystem(String fileName) throws IOException {
         byte[] images = Files.readAllBytes(new File("/var/www/vuary/types/"+fileName).toPath());
