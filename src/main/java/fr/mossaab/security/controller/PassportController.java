863a7b5c91131c411448be05019a6a2e9d66067b
Kichmarevitmo <kichmarev2003@gmail.com>
2024-06-05 00:17:58 +0300

    done

--- src/main/java/fr/mossaab/security/controller/PassportController.java
+++ src/main/java/fr/mossaab/security/controller/PassportController.java
@@ -26,7 +26,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping(value = "/passport", produces = "application/json;charset=UTF-8", consumes = "application/json;charset=UTF-8")
+@RequestMapping("/passport")
 @RequiredArgsConstructor
 public class PassportController {
 
