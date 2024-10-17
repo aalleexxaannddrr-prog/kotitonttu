149529c058264a7fc4dba1b87adc0736135db84d
Kichmarevitmo <kichmarev2003@gmail.com>
2024-10-17 05:01:44 +0300

    fix

--- src/main/java/fr/mossaab/security/entities/FileData.java
+++ src/main/java/fr/mossaab/security/entities/FileData.java
@@ -45,26 +45,26 @@ public class FileData {
     /**
      * Пользователь, связанный с файлом.
      */
-    @OneToOne(optional = false)
-    @JoinColumn(name = "_user_id", referencedColumnName = "id", unique = true)
+    @OneToOne(optional = true)
+    @JoinColumn(name = "_user_id", referencedColumnName = "id", unique = true, nullable = true)
     @OnDelete(action = OnDeleteAction.CASCADE)
     @JsonBackReference
     private User user;
 
-    @ManyToOne(optional = false, fetch = FetchType.LAZY)
-    @JoinColumn(name = "bonus_request_id", nullable = false)
+    @ManyToOne(optional = true, fetch = FetchType.LAZY)
+    @JoinColumn(name = "bonus_request_id", nullable = true)
     @OnDelete(action = OnDeleteAction.CASCADE)
     @JsonBackReference
     private BonusRequest bonusRequest;
 
-    @ManyToOne(optional = false, fetch = FetchType.LAZY)
-    @JoinColumn(name = "document_title_id", nullable = false)
+    @ManyToOne(optional = true, fetch = FetchType.LAZY)
+    @JoinColumn(name = "document_title_id", nullable = true)
     @OnDelete(action = OnDeleteAction.CASCADE)
     @JsonBackReference
     private DocumentTitle documentTitle;
 
-    @ManyToOne(optional = false, fetch = FetchType.LAZY)
-    @JoinColumn(name = "passport_title_id", nullable = false)
+    @ManyToOne(optional = true, fetch = FetchType.LAZY)
+    @JoinColumn(name = "passport_title_id", nullable = true)
     @OnDelete(action = OnDeleteAction.CASCADE)
     @JsonBackReference
     private PassportTitle passportTitle;
@@ -72,8 +72,8 @@ public class FileData {
     /**
      * Родительская сущность Series, связанная с файлом.
      */
-    @ManyToOne(optional = false, fetch = FetchType.LAZY)
-    @JoinColumn(name = "series_id", nullable = false)
+    @ManyToOne(optional = true, fetch = FetchType.LAZY)
+    @JoinColumn(name = "series_id", nullable = true)
     @OnDelete(action = OnDeleteAction.CASCADE)
     @JsonBackReference
     private Series series;
